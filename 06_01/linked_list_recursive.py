b5caee9c7b18cb5a177f50802aef4f6fcac22b05
unknown <prandrews@hotmail.co.uk>
2020-12-05 10:03:34 +0000

    Recorded successfully

--- 06_01/linked_list_recursive.py
+++ 06_01/linked_list_recursive.py
@@ -5,4 +5,27 @@ Robin Andrews - https://compucademy.net/
 
 
 class Node:
-    pass
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+def traverse(head):
+    # Base case
+    if head is None:
+        return
+    # Recursive case
+    print(head.data)
+    traverse(head.next)
+
+
+item1 = Node("dog")
+item2 = Node("cat")
+item3 = Node("rat")
+item1.next = item2
+item2.next = item3
+
+# traverse(item1)
+
+head = Node("dog", Node("cat", Node("rat", None)))
+traverse(head)
