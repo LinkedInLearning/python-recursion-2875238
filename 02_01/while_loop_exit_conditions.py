a8555ea59c2e00e3210d686e5ef25dc4c829900e
Hasan Ali <92353024+Hasa9nAli@users.noreply.github.com>
2022-06-14 03:31:31 +0300

    base case

--- 02_01/while_loop_exit_conditions.py
+++ 02_01/while_loop_exit_conditions.py
@@ -4,10 +4,21 @@ Robin Andrews - https://compucademy.net/
 """
 
 # No base case, no movement towards base case.
-
+count = 1 
+while True:   
+  print(count)
 
 # We have a base case, but no movement towards it.
-
-
+count = 1 
+while True: 
+  if count >10 : 
+    break 
+  print(count)
+ 
 # Base Case and movement towards it.
-
+count = 1 
+while True: 
+  if count >10 : 
+    break 
+  print(count)
+  count += 1
